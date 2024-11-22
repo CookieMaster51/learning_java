73dd4bb650e05fc5186779a381a68bec1a1f8dee
Cookiemaster51 <jan.matis.2008@gmail.com>
2024-11-22 22:37:10 +0000

    comments ndat

--- src/coolExercises/PrimeNumbers.java
+++ src/coolExercises/PrimeNumbers.java
@@ -4,33 +4,37 @@ import java.util.Arrays;
 import java.util.concurrent.CountDownLatch;
 
 public class PrimeNumbers {
-    public static int numThreads = 15;
-    public static long[] primeSums = new long[numThreads];
+    public static int numThreads = 15; // Best on my machine, change for different CPUs
+    public static long[] primeSums = new long[numThreads]; // To allow an arbitrary number of threads to return values
+
     public static void main(String[] args) {
-        long maximum = (long) 1e6; // Change this for testing
+        long maximum = (long) 1e6;
         long totalElapsed = 0;
-        for (int i = 0; i < 100; i++) {
+
+        for (int i = 0; i < 10; i++) { // to run through 10 times to get an average time
             long startTime = System.currentTimeMillis();
+
             long result = 0;
 
             CountDownLatch countDownLatch = new CountDownLatch(numThreads);
-            for (int j = numThreads-1; j > -1; j--){
+            for (int threadIndex = numThreads-1; threadIndex > -1; threadIndex--){ // Starts with the biggest one
                 // System.out.println(nextMulSix((j)*maximum/numThreads) + " " + nextMulSix((j + 1)*maximum/numThreads));
-                PrimeSumTask task = new PrimeSumTask(countDownLatch, nextMulSix(j*maximum/numThreads),
-                        nextMulSix((j+1)*maximum/numThreads)-1, j);
-                Thread thread = new Thread(task);
-                thread.start();
+                PrimeSumTask task = new PrimeSumTask(countDownLatch, nextMulSix(threadIndex*maximum/numThreads),
+                        nextMulSix((threadIndex+1)*maximum/numThreads)-1, threadIndex); // Makes a new runnable task
+
+                Thread thread = new Thread(task); // Assigns the task to a thread
+                thread.start(); // Starts the thread
             }
             
             try {
-                countDownLatch.await();
-                result = 5 + Arrays.stream(primeSums).sum();
+                countDownLatch.await(); // Waits for them to finish
+                result = 5 + Arrays.stream(primeSums).sum(); // 5 because 2 and 3 are not counted
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
             long stopTime = System.currentTimeMillis();
-            if (result != 37550402023L){
+            if (result != 37550402023L){ // Precalculated expected result
                 System.out.println("FAIL");
             }
             long elapsed = stopTime - startTime;
@@ -60,14 +64,14 @@ class PrimeSumTask implements Runnable{
     }
 
     @Override
-    public void run() {
+    public void run() { // This is what thread.start() calls
         PrimeNumbers.primeSums[position] = getSumOfPrimes(start, end);
         countDownLatch.countDown();
     }
     
     public static long getSumOfPrimes(long start, long maximum){
         long total = 0;
-        for (long possPrime = start; possPrime <= maximum; possPrime+=6){
+        for (long possPrime = start; possPrime <= maximum; possPrime+=6){ // Every prime is one above or one below a multiple of six
             if(isPrime(possPrime - 1)){
                 total += possPrime-1;
             }
@@ -82,12 +86,12 @@ class PrimeSumTask implements Runnable{
         long max = (long) Math.sqrt(toCheck) + 1;
         long currCheck = 6;
         boolean found = false;
-        while (currCheck <= max && !found) {
+        while (currCheck <= max && !found) { // Again only need to check primes so only check one above and one below a multiple of six
             if (toCheck % (currCheck-1) == 0) {
                 return false;
             }
             if (toCheck % (currCheck+1) == 0) {
-                return false;
+                return false; // Ok, ik im not supposed to do this, but it is much quicker
             }
             currCheck += 6;
         }
