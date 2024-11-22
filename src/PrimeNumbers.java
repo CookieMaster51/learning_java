133d46a8a8d6277fc00a75c54022cd675a0c1167
Cookiemaster51 <jan.matis.2008@gmail.com>
2024-11-22 21:30:02 +0000

    multithreaded :)

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -1,10 +1,27 @@
+import java.util.concurrent.CountDownLatch;
+
 public class PrimeNumbers {
     public static void main(String[] args) {
         long maximum = (long) 1e6; // Change this for testing
         long totalElapsed = 0;
         for (int i = 0; i < 10; i++) {
             long startTime = System.currentTimeMillis();
-            long result = 5 + getSumOfPrimes(6, maximum/2) + getSumOfPrimes(nextMulSix(maximum/2), maximum);
+            long result = 0;
+            CountDownLatch countDownLatch = new CountDownLatch(2);
+            PrimeSumTask task1 = new PrimeSumTask(countDownLatch, 6, 2*maximum/3);
+            PrimeSumTask task2 = new PrimeSumTask(countDownLatch, nextMulSix(2*maximum/3), maximum);
+            Thread thread = new Thread(task1);
+            Thread thread2 = new Thread(task2);
+            thread.start();
+            thread2.start();
+            
+            try {
+                countDownLatch.await();
+                result = 5 + task1.sum + task2.sum;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
             long stopTime = System.currentTimeMillis();
             if (result != 37550402023L){
                 System.out.println("FAIL");
@@ -15,6 +32,31 @@ public class PrimeNumbers {
         long elapsed = totalElapsed / 10;
         System.out.printf("The function took %d milliseconds", elapsed);
     }
+
+
+    public static long nextMulSix(long maximum){
+        return (maximum + 6 - (maximum % 6));
+    }
+}
+
+class PrimeSumTask implements Runnable{
+    CountDownLatch countDownLatch;
+    long start;
+    long end;
+    long sum;
+    
+    public PrimeSumTask(CountDownLatch countDownLatch, long start, long end){
+        this.countDownLatch = countDownLatch;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public void run() {
+        this.sum = getSumOfPrimes(start, end);
+        countDownLatch.countDown();
+    }
+    
     public static long getSumOfPrimes(long start, long maximum){
         long total = 0;
         for (long possPrime = start; possPrime <= maximum; possPrime+=6){
@@ -43,9 +85,4 @@ public class PrimeNumbers {
         }
         return toReturn;
     }
-
-    public static long nextMulSix(long maximum){
-        return (maximum + 6 - (maximum % 6));
-    }
 }
-
